007393d12c192ce7c63c25ad7ce14c2aca95913e
frostraver <drieslamberechts@gmail.Com>
2013-02-19 17:42:39 +0100

    Dries: DriesScene

--- XNA_ENGINE/Game/Scenes/DriesScene.cs
+++ XNA_ENGINE/Game/Scenes/DriesScene.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using XNA_ENGINE.Engine.Objects;
+using XNA_ENGINE.Engine.Scenegraph;
+using XNA_ENGINE.Game.Objects;
+using VampirePuzzle.Framework;
 
 namespace XNA_ENGINE.Game.Scenes
 {
-    class DriesScene
+    class DriesScene : GameScene
     {
+        public DriesScene() : base("DriesScene") { }
+
+        public override void Initialize()
+        {
+        }
     }
 }
