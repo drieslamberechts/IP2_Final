c26de03c2bbf69a35fe9bc80f472f7ca74d6f5d8
woutdp <woutdp@telenet.be>
2013-02-21 23:54:54 +0100

    Wout: Fiddles around with some stuff to test

--- XNA_ENGINE/Game/Scenes/WoutScene.cs
+++ XNA_ENGINE/Game/Scenes/WoutScene.cs
@@ -6,6 +6,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using XNA_ENGINE.Engine.Objects;
+using XNA_ENGINE.Engine;
 using XNA_ENGINE.Engine.Scenegraph;
 using XNA_ENGINE.Game.Objects;
 using VampirePuzzle.Framework;
@@ -20,9 +21,20 @@ namespace XNA_ENGINE.Game.Scenes
 
         public override void Initialize()
         {
-            m_SmileySprite = new GameSprite("TestSmiley");
-            m_SmileySprite.Translate(0, 0);
+            m_SmileySprite = new GameSprite("protagtransparant");
+            m_SmileySprite.Translate(100,100);
+            m_SmileySprite.Scale(0.2f, 0.2f);
+            m_SmileySprite.Rotate(45);
             AddSceneObject(m_SmileySprite);
+        }
+        
+        public override void Update(RenderContext renderContext)
+        {
+           // m_SmileySprite.Translate(-m_SmileySprite.Width / 5 / 2, -m_SmileySprite.Width / 5 / 2);
+            //m_SmileySprite.Rotate(180);
+            
+           // m_SmileySprite.Rotate(renderContext.GameTime.TotalGameTime);
+            base.Update(renderContext);
         }
     }
 }
