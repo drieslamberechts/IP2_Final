52ee3beb64fc916bbd61117a0f5a122c524d45d2
woutdp <woutdp@telenet.be>
2013-02-26 16:24:37 +0100

    Wout: fix

--- XNA_ENGINE/Game/Scenes/InteractionScene.cs
+++ XNA_ENGINE/Game/Scenes/InteractionScene.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Content;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,7 +15,7 @@ namespace XNA_ENGINE.Game.Scenes
         public InteractionScene()
             :base("InteractionScene")
         {
-            Content = Content;
+            Content = content;
         }
     }
 }
