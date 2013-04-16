aa3fa6c7661297e56f652ddb93c34ddb6ab5a229
woutdp <woutdp@telenet.be>
2013-04-16 15:08:43 +0200

    Xml files get saved in the content folder

--- XNA_ENGINE/Game/Helpers/MapLoadSave.cs
+++ XNA_ENGINE/Game/Helpers/MapLoadSave.cs
@@ -42,7 +42,7 @@ namespace XNA_ENGINE.Game.Helpers
         {
             GridTile[,] gridField = new GridTile[MAX_ROWS, MAX_COLUMNS];
 
-            Stream stream = TitleContainer.OpenStream("./" + fileName + ".xml");
+            Stream stream = TitleContainer.OpenStream(".../.../.../.../XNA_DEMOContent/XMLFiles/" + fileName + ".xml");
             XDocument doc = XDocument.Load(stream);
             
             List<GridTile> gridList = (from tile in doc.Descendants("tile")
@@ -62,7 +62,7 @@ namespace XNA_ENGINE.Game.Helpers
         public void SaveMap(GridTile[,] gridField, string fileName = "GeneratedTileMap")
         {
             //"../../../../XNA_DEMOContent/XMLFiles/" + 
-            var xmlFile = new FileStream("./" + fileName + ".xml", FileMode.OpenOrCreate, FileAccess.Write);
+            var xmlFile = new FileStream("../../../../XNA_DEMOContent/XMLFiles/" + fileName + ".xml", FileMode.OpenOrCreate, FileAccess.Write);
             var writer = new StreamWriter(xmlFile);
 
             writer.WriteLine("<?xml version=\"1.0\" encoding=\"utf-8\" ?>");
