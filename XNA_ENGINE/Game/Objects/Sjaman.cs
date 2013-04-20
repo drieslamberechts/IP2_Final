569f03db9aced47f5610b6d8ba5bb4f408f35a12
frostraver <drieslamberechts@gmail.Com>
2013-04-20 17:05:35 +0200

    Commit Sjaman

--- XNA_ENGINE/Game/Objects/Sjaman.cs
+++ XNA_ENGINE/Game/Objects/Sjaman.cs
@@ -2,10 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace XNA_ENGINE.Game.Objects
 {
-    class Sjaman
+    class Sjaman : Placeable
     {
+        private Vector2 m_Position;
+
+        public Sjaman()
+        {
+            m_Position = new Vector2(0, 0);
+        }
+
+        public void SetPosition(Vector2 position)
+        {
+            m_Position = position;
+        }
+
+        public Vector2 GetPosition()
+        {
+            return m_Position;
+        }
     }
 }
