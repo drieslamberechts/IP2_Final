c0a8d9e7a6419f9ff03b09150f686b8708463350
frostraver <drieslamberechts@gmail.Com>
2013-05-21 16:48:18 +0200

    Commit Background

--- XNA_ENGINE/Game/Scenes/MainMenuScene.cs
+++ XNA_ENGINE/Game/Scenes/MainMenuScene.cs
@@ -77,7 +77,7 @@ namespace XNA_ENGINE.Game.Scenes
 
         public override void Draw2D(RenderContext renderContext, bool drawBefore3D)
         {
-            // text
+            // text2
             int gameHeight = renderContext.GraphicsDevice.Viewport.Height;
             int gameWidth = renderContext.GraphicsDevice.Viewport.Width;
 
