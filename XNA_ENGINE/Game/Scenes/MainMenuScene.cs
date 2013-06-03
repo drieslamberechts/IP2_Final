2232b897fb370bf13c1915311e14c8b037cdfea5
frostraver <drieslamberechts@gmail.Com>
2013-06-03 15:15:08 +0200

    Added Controls Button in Main Menu

--- XNA_ENGINE/Game/Scenes/MainMenuScene.cs
+++ XNA_ENGINE/Game/Scenes/MainMenuScene.cs
@@ -28,17 +28,39 @@ namespace XNA_ENGINE.Game.Scenes
         private Texture2D m_ExitButtonHovered;
         private Texture2D m_DaeScreen;
         private Texture2D m_BackgroundTexture;
+        private Texture2D m_ButtonControls, m_ButtonControlsHovered;
 
         private Rectangle m_StartRect;
         private Rectangle m_ExitRect;
         private Rectangle m_DaeRect;
         private Rectangle m_BackgroundRect;
+        private Rectangle m_RectButtonControls;
         private InputManager m_InputManager;
         private bool m_ExitGame;
         private  int m_Counter;
 
         private bool m_bStartButtonHovered;
         private bool m_bExitButtonHovered;
+        private bool m_bControlsHovered;
+
+        // Variables Controls Menu
+        private bool m_bShowControls, m_bButtonReturnHovered, m_bShowControllerLayout, m_bShowKeyboardLayout;
+        private bool m_bLayoutSwitch;
+
+        private Texture2D m_TexControlsBackground,
+                          m_TexControlsKeyboard,
+                          m_TexControlsController,
+                          m_TexButtonReturn,
+                          m_TexButtonReturnHovered,
+                          m_TexButtonControllerSwitch,
+                          m_TexButtonKeyboardSwitch;
+
+        private Rectangle m_RectControlsBackground,
+                          m_RectControlsKeyboard,
+                          m_RectControlsController,
+                          m_RectButtonReturn,
+                          m_RectButtonControllerSwitch,
+                          m_RectButtonKeyboardSwitch;
 
         private const int SPLASHSCREENTIME = 200;
 
@@ -53,6 +75,14 @@ namespace XNA_ENGINE.Game.Scenes
         {
             m_bStartButtonHovered = false;
             m_bExitButtonHovered = false;
+            m_bControlsHovered = false;
+            m_bShowControls = false;
+
+            m_bShowControls = false;
+            m_bButtonReturnHovered = false;
+            m_bShowControllerLayout = true;
+            m_bShowKeyboardLayout = false;
+            m_bLayoutSwitch = true;
 
             // background
             m_BackgroundTexture = m_Content.Load<Texture2D>("final Menu/Background_StartMenu");
@@ -66,6 +96,17 @@ namespace XNA_ENGINE.Game.Scenes
             m_ExitButton = m_Content.Load<Texture2D>("final Menu/Button_StartMenu_ExitButtonUnhovered");
             m_StartButtonHovered = m_Content.Load<Texture2D>("final Menu/Button_StartMenu_StartButtonHovered");
             m_ExitButtonHovered = m_Content.Load<Texture2D>("final Menu/Button_StartMenu_ExitButtonHovered");
+            m_ButtonControls = m_Content.Load<Texture2D>("pause Menu/Button_ControlsUnhovered");
+            m_ButtonControlsHovered = m_Content.Load<Texture2D>("pause Menu/Button_ControlsHovered");
+
+            // CONTROLLER MENU
+            m_TexControlsBackground = m_Content.Load<Texture2D>("ControllerMenu/Controller_Background");
+            m_TexControlsKeyboard = m_Content.Load<Texture2D>("ControllerMenu/Controller_MouseAndKeyboard");
+            m_TexControlsController = m_Content.Load<Texture2D>("ControllerMenu/Controller_xboxController");
+            m_TexButtonReturn = m_Content.Load<Texture2D>("ControllerMenu/Controller_ReturnButtonUnhovered");
+            m_TexButtonReturnHovered = m_Content.Load<Texture2D>("ControllerMenu/Controller_ReturnButtonHovered");
+            m_TexButtonControllerSwitch = m_Content.Load<Texture2D>("ControllerMenu/Controller_xboxControllerButton");
+            m_TexButtonKeyboardSwitch = m_Content.Load<Texture2D>("ControllerMenu/Controller_MouseAndKeyboardButton");
 
             // InputManager
             m_InputManager = new InputManager();
@@ -93,12 +134,30 @@ namespace XNA_ENGINE.Game.Scenes
             m_DaeRect = new Rectangle(0, 0, gameWidth, gameHeight);
             m_StartRect = new Rectangle(gameWidth / 2 - m_StartButton.Width / 2, gameHeight / 2 - 175, m_StartButton.Width, m_StartButton.Height);
             m_ExitRect = new Rectangle(gameWidth / 2 - m_ExitButton.Width / 2, gameHeight / 2, m_ExitButton.Width, m_ExitButton.Height);
+            m_RectButtonControls = new Rectangle(gameWidth - m_ButtonControls.Width - 50, gameHeight - m_ButtonControls.Height - 50, m_ButtonControls.Width, m_ButtonControls.Height);
             m_BackgroundRect = new Rectangle(0, 0, gameWidth, gameHeight);
+
+            // CONTROLLER MENU
+            m_RectControlsBackground = new Rectangle(gameWidth / 2 - m_TexControlsBackground.Width / 2, 40, m_TexControlsBackground.Width, m_TexControlsBackground.Height);
+            m_RectControlsKeyboard = new Rectangle(gameWidth / 2 - m_TexControlsKeyboard.Width / 2, 300, m_TexControlsKeyboard.Width, m_TexControlsKeyboard.Height);
+            m_RectControlsController = new Rectangle(gameWidth / 2 - m_TexControlsController.Width / 2, 300, m_TexControlsController.Width, m_TexControlsController.Height);
+            m_RectButtonReturn = new Rectangle(gameWidth / 2 - 600, 900, m_TexButtonReturn.Width, m_TexButtonReturn.Height);
+            m_RectButtonControllerSwitch = new Rectangle(gameWidth / 2 - m_TexButtonControllerSwitch.Width / 2, 160, m_TexButtonControllerSwitch.Width, m_TexButtonControllerSwitch.Height);
+            m_RectButtonKeyboardSwitch = new Rectangle(gameWidth / 2 - m_TexButtonKeyboardSwitch.Width / 2, 160, m_TexButtonKeyboardSwitch.Width, m_TexButtonKeyboardSwitch.Height);
 
             if (gameWidth < renderContext.GraphicsDevice.Adapter.CurrentDisplayMode.Width)
             {
                 m_StartRect = new Rectangle(gameWidth / 2 - m_StartButton.Width / 4, gameHeight / 2 - 85, m_StartButton.Width / 2, m_StartButton.Height / 2);
                 m_ExitRect = new Rectangle(gameWidth / 2 - m_ExitButton.Width / 4, gameHeight / 2, m_ExitButton.Width / 2, m_ExitButton.Height / 2);
+                m_RectButtonControls = new Rectangle(gameWidth - m_ButtonControls.Width / 2 - 25, gameHeight - m_ButtonControls.Height / 2 - 25, m_ButtonControls.Width / 2, m_ButtonControls.Height / 2);
+
+                // CONTROLLER MENU
+                m_RectControlsBackground = new Rectangle(gameWidth / 2 - m_TexControlsBackground.Width / 4, 20, m_TexControlsBackground.Width / 2, m_TexControlsBackground.Height / 2);
+                m_RectControlsKeyboard = new Rectangle(gameWidth / 2 - m_TexControlsKeyboard.Width / 4, 150, m_TexControlsKeyboard.Width / 2, m_TexControlsKeyboard.Height / 2);
+                m_RectControlsController = new Rectangle(gameWidth / 2 - m_TexControlsController.Width / 4, 150, m_TexControlsController.Width / 2, m_TexControlsController.Height / 2);
+                m_RectButtonReturn = new Rectangle(gameWidth / 2 - 300, 450, m_TexButtonReturn.Width / 2, m_TexButtonReturn.Height / 2);
+                m_RectButtonControllerSwitch = new Rectangle(gameWidth / 2 - m_TexButtonControllerSwitch.Width / 4, 70, m_TexButtonControllerSwitch.Width / 2, m_TexButtonControllerSwitch.Height / 2);
+                m_RectButtonKeyboardSwitch = new Rectangle(gameWidth / 2 - m_TexButtonKeyboardSwitch.Width / 4, 70, m_TexButtonKeyboardSwitch.Width / 2, m_TexButtonKeyboardSwitch.Height / 2);
             }
 
             if (m_Counter < SPLASHSCREENTIME)
@@ -119,6 +178,32 @@ namespace XNA_ENGINE.Game.Scenes
                     renderContext.SpriteBatch.Draw(m_ExitButton, m_ExitRect, Color.White);
                 else
                     renderContext.SpriteBatch.Draw(m_ExitButtonHovered, m_ExitRect, Color.White);
+
+                if (m_bControlsHovered)
+                    renderContext.SpriteBatch.Draw(m_ButtonControlsHovered, m_RectButtonControls, Color.White);
+                else renderContext.SpriteBatch.Draw(m_ButtonControls, m_RectButtonControls, Color.White);
+            }
+
+            // DRAW CONTROLLER MENU
+            if (m_bShowControls)
+            {
+                renderContext.SpriteBatch.Draw(m_TexControlsBackground, m_RectControlsBackground, Color.White);
+
+                if (m_bShowControllerLayout)
+                {
+                    renderContext.SpriteBatch.Draw(m_TexButtonControllerSwitch, m_RectButtonControllerSwitch, Color.White);
+                    renderContext.SpriteBatch.Draw(m_TexControlsController, m_RectControlsController, Color.White);
+                }
+                else if (m_bShowKeyboardLayout)
+                {
+                    renderContext.SpriteBatch.Draw(m_TexButtonKeyboardSwitch, m_RectButtonKeyboardSwitch, Color.White);
+                    renderContext.SpriteBatch.Draw(m_TexControlsKeyboard, m_RectControlsKeyboard, Color.White);
+                }
+
+                // Return Button
+                if (m_bButtonReturnHovered)
+                    renderContext.SpriteBatch.Draw(m_TexButtonReturnHovered, m_RectButtonReturn, Color.White);
+                else renderContext.SpriteBatch.Draw(m_TexButtonReturn, m_RectButtonReturn, Color.White);
             }
 
             base.Draw2D(renderContext, drawBefore3D);
@@ -147,6 +232,38 @@ namespace XNA_ENGINE.Game.Scenes
             {
                 m_bExitButtonHovered = false;
             }
+
+            if (CheckHitButton(mousePos, m_RectButtonControls))
+                m_bControlsHovered = true;
+            else
+                m_bControlsHovered = false;
+
+            if (m_InputManager.GetAction((int)PlayerInput.LeftClick).IsTriggered && CheckHitButton(mousePos, m_RectButtonControls))
+                m_bShowControls = true;
+
+            if (CheckHitButton(mousePos, m_RectButtonReturn) && m_bShowControls)
+                m_bButtonReturnHovered = true;
+            else m_bButtonReturnHovered = false;
+
+            // CONTROLS
+            if (m_InputManager.GetAction((int)PlayScene.PlayerInput.LeftClick).IsTriggered && CheckHitButton(mousePos, m_RectButtonReturn) && m_bShowControls)
+                m_bShowControls = false;
+
+            if (m_InputManager.GetAction((int)PlayScene.PlayerInput.LeftClick).IsTriggered && CheckHitButton(mousePos, m_RectButtonControllerSwitch) && m_bShowControls && m_bShowControllerLayout && m_bLayoutSwitch)
+            {
+                m_bShowControllerLayout = false;
+                m_bShowKeyboardLayout = true;
+                m_bLayoutSwitch = false;
+            }
+
+            if (m_InputManager.GetAction((int)PlayScene.PlayerInput.LeftClick).IsTriggered && CheckHitButton(mousePos, m_RectButtonKeyboardSwitch) && m_bShowControls && m_bShowKeyboardLayout && m_bLayoutSwitch)
+            {
+                m_bShowKeyboardLayout = false;
+                m_bShowControllerLayout = true;
+                m_bLayoutSwitch = false;
+            }
+
+            m_bLayoutSwitch = true;
 
             if (m_InputManager.GetAction((int)PlayerInput.LeftClick).IsTriggered && CheckHitButton(mousePos, m_StartRect))
             {
